45392b46ca82a73ebdee76c8eab05614b9bb41e8
Darkness_696 <darkness_696@mail.ru>
2025-11-09 17:33:05 +0300

    registered RepositoryContext at a Runtime

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts;
 using LoggerService;
+using Microsoft.EntityFrameworkCore;
 using Repository;
 using Service;
 using Service.Contracts;
@@ -20,7 +21,7 @@ namespace CompanyEmployees.Extensions
         public static void ConfigureIISIntegration(this IServiceCollection services) =>
             services.Configure<IISOptions>(options =>
             {
-            
+
             });
 
         public static void ConfigureLoggerService(this IServiceCollection services) =>
@@ -31,5 +32,9 @@ namespace CompanyEmployees.Extensions
 
         public static void ConfigureServiceManager(this IServiceCollection services) =>
             services.AddScoped<IServiceManager, ServiceManager>();
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<RepositoryContext>(opts =>
+                opts.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
     }
 }
