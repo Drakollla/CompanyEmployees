6ac165af18f62a1562b6cefab962d85d15145e75
Darkness_696 <darkness_696@mail.ru>
2025-11-13 13:12:38 +0300

    added validation for create request

--- CompanyEmployees.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees.Presentation/Controllers/EmployeesController.cs
@@ -34,6 +34,9 @@ namespace CompanyEmployees.Presentation.Controllers
             if (employee is null)
                 return BadRequest("EmployeeCreationDto object is null");
 
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
             var employeeToReturn = _service.EmployeeService.CreateEmployeeForCompany(companyId, employee, trackChanges: false);
 
             return CreatedAtRoute("GetEmployeeForCompany", new { companyId, id = employeeToReturn.Id }, employeeToReturn);
