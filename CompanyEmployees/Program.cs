2c23857d3504defb7ad5d9864949a6f0f3a1483f
Darkness_696 <darkness_696@mail.ru>
2025-11-20 10:23:07 +0300

    added apiversion

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -19,6 +19,7 @@ builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.ConfigureVersioning();
 
 builder.Services.AddScoped<IDataShaper<EmployeeDto>, DataShaper<EmployeeDto>>();
 builder.Services.AddScoped<ValidationFilterAttribute>();
