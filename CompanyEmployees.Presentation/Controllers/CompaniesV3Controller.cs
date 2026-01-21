2f19df8c1cab1feb0899e2190f5979383f6ba897
Darkness_696 <darkness_696@mail.ru>
2026-01-21 13:03:04 +0300

    added delete command

--- CompanyEmployees.Presentation/Controllers/CompaniesV3Controller.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesV3Controller.cs
@@ -1,62 +1,70 @@
-﻿//using Application.Commands;
-//using Application.Queries;
-//using MediatR;
-//using Microsoft.AspNetCore.Mvc;
-//using Shared.DataTransferObjects;
+﻿using Application.Commands;
+using Application.Notifications;
+using Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Shared.DataTransferObjects;
 
-//namespace CompanyEmployees.Presentation.Controllers
-//{
-//    [Route("api/v3/companies")]
-//    [ApiController]
-//    public class CompaniesV3Controller : ControllerBase
-//    {
-//        private readonly ISender _sender;
-//        public CompaniesV3Controller(ISender sender) => _sender = sender;
+namespace CompanyEmployees.Presentation.Controllers
+{
+    [ApiVersion("3.0")]
+    [Route("api/v3/companies")]
+    [ApiController]
+    public class CompaniesV3Controller : ControllerBase
+    {
+        private readonly ISender _sender;
+        private readonly IPublisher _publisher;
 
-//        [HttpGet]
-//        public async Task<IActionResult> GetCompanies()
-//        {
-//            var companies = await _sender.Send(new GetCompaniesQuery(TrackChanges: false));
-            
-//            return Ok(companies);
-//        }
+        public CompaniesV3Controller(ISender sender, IPublisher publisher)
+        {
+            _sender = sender;
+            _publisher = publisher;
+        }
 
-//        [HttpGet("{id:guid}", Name = "CompanyById")]
-//        public async Task<IActionResult> GetCompany(Guid id)
-//        {
-//            var company = await _sender.Send(new GetCompanyQuery(id, TrackChanges: false));
-            
-//            return Ok(company);
-//        }
+        [HttpGet]
+        public async Task<IActionResult> GetCompanies()
+        {
+            var companies = await _sender.Send(new GetCompaniesQuery(TrackChanges: false));
 
-//        [HttpPost]
-//        public async Task<IActionResult> CreateCompany([FromBody] CompanyForCreationDto companyForCreationDto)
-//        {
-//            if (companyForCreationDto is null)
-//                return BadRequest("CompanyForCreationDto object is null");
+            return Ok(companies);
+        }
 
-//            var company = await _sender.Send(new CreateCompanyCommand(companyForCreationDto));
+        [HttpGet("{id:guid}", Name = "CompanyById")]
+        public async Task<IActionResult> GetCompany(Guid id)
+        {
+            var company = await _sender.Send(new GetCompanyQuery(id, TrackChanges: false));
 
-//            return CreatedAtRoute("CompanyById", new { Id = company.Id }, company);
-//        }
+            return Ok(company);
+        }
 
-//        [HttpPut]
-//        public async Task<IActionResult> UpdateCompany(Guid id, CompanyForUpdateDto companyForUpdateDto)
-//        {
-//            if (companyForUpdateDto is null)
-//                return BadRequest("CompanyForUpdateDto object is null");
+        [HttpPost]
+        public async Task<IActionResult> CreateCompany([FromBody] CompanyForCreationDto companyForCreationDto)
+        {
+            if (companyForCreationDto is null)
+                return BadRequest("CompanyForCreationDto object is null");
 
-//            await _sender.Send(new UpdateCompanyCommand(id, companyForUpdateDto, TrackChanges: true));
-//            return NoContent();
-//        }
+            var company = await _sender.Send(new CreateCompanyCommand(companyForCreationDto));
 
-//        [HttpDelete("{id:guid}")]
-//        public async Task<IActionResult> DeleteCompany(Guid id)
-//        {
-//            await _sender.Send(new DeleteCompanyCommand(id, TrackChanges: false));
+            return CreatedAtRoute("CompanyById", new { Id = company.Id }, company);
+        }
 
-//            return NoContent();
-//        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateCompany(Guid id, CompanyForUpdateDto companyForUpdateDto)
+        {
+            if (companyForUpdateDto is null)
+                return BadRequest("CompanyForUpdateDto object is null");
 
-//    }
-//}
+            await _sender.Send(new UpdateCompanyCommand(id, companyForUpdateDto, TrackChanges: true));
+            return NoContent();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteCompany(Guid id)
+        {
+            await _publisher.Publish(new CompanyDeletedNotification(id, TrackChanges: false)); 
+
+            return NoContent();
+        }
+
+    }
+}
